f5a5beaaddee0cdd6366184216188ebec81948d0
Juan Fernando Álvarez Gallego <alvarezjfernandog@gmail.com>
2024-11-26 00:03:59 -0500

    bugfix: logos fixed

--- src/const/technologies.ts
+++ src/const/technologies.ts
@@ -3,90 +3,91 @@ import type { ITechnologies } from "../interfaces/ITechnologies";
 export const TECHNOLOGIES: ITechnologies[] = [
     {
         name: "Angular",
-        image: "./../assets/icons/Angular.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Angular.svg"
     },
     {
         name: "Astro",
-        image: "./../assets/icons/Astro.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Astro.svg"
     },
     {
         name: "AWS",
-        image: "./../assets/icons/AWS.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/AWS.svg"
     },
     {
         name: "Bootstrap",
-        image: "./../assets/icons/Bootstrap.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Bootstrap.svg"
     },
     {
         name: "C#",
-        image: "./../assets/icons/C# (CSharp).svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/C%23%20(CSharp).svg"
     },
     {
         name: "CSS3",
-        image: "./../assets/icons/CSS3.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/CSS3.svg"
     },
     {
         name: "Firebase",
-        image: "./../assets/icons/Firebase.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Firebase.svg"
     },
     {
         name: "Gatsby",
-        image: "./../assets/icons/Gatsby.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Gatsby.svg"
     },
     {
         name: "HTML5",
-        image: "./../assets/icons/HTML5.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/HTML5.svg"
     },
     {
         name: "Java",
-        image: "./../assets/icons/Java.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Java.svg"
     },
     {
         name: "JavaScript",
-        image: "./../assets/icons/JavaScript.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/JavaScript.svg"
     },
     {
         name: "Material UI",
-        image: "./../assets/icons/Material UI.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Material%20UI.svg"
     },
     {
         name: "MongoDB",
-        image: "./../assets/icons/MongoDB.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/MongoDB.svg"
     },
     {
         name: "MySQL",
-        image: "./../assets/icons/MySQL.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/MySQL.svg"
     },
     {
         name: "NestJs",
-        image: "./../assets/icons/Nest.js.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Nest.js.svg"
     },
     {
         name: "NextJs",
-        image: "./../assets/icons/Next.js.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Next.js.svg"
     },
     {
         name: "NodeJs",
-        image: "./../assets/icons/Node.js.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Node.js.svg"
     },
     {
         name: "PostgreSQL",
-        image: "./../assets/icons/PostgreSQL.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/PostgresSQL.svg"
     },
     {
         name: "React",
-        image: "./../assets/icons/React.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/React.svg"
     },
     {
         name: "Sass",
-        image: "./../assets/icons/Sass.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Sass.svg"
     },
     {
         name: "Tailwind CSS",
-        image: "./../assets/icons/Tailwind CSS.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Tailwind%20CSS.svg"
     },
     {
         name: "TypeScript",
-        image: "./../assets/icons/TypeScript.svg"
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/TypeScript.svg"
     }
 ];
+
