5cfa62e37bfb8136a94657cf408ceb22fe0c8ed4
Juanpa0108 <jrobayomaestre@gmail.com>
2024-11-27 21:12:12 -0500

    Feature: BasicInfo and Contactpage full

---
+++ src/const/basicInfo.ts
@@ -0,0 +1,19 @@
+import type { BasicInfo } from "../interfaces/BasicInfo";
+
+export const BASICINFO: BasicInfo[] =[
+    {
+        name : "Juan Fernando Álvarez Gallego",
+        email : "alvarezjfernandog@gmail.com",
+        phoneNumber: 3022856079
+    },
+    {
+        name : "Juan Pablo Robayo",
+        email : "jrobayomaestre@gmail.com",
+        phoneNumber: 3178287138
+    },
+    {
+        name : "Juan Manuel De la Torre Suaréz",
+        email : "delatorresuar0210@gmail.com",
+        phoneNumber: 3215235657
+    }
+]
