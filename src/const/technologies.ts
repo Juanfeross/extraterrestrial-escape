f4973e85ce43c38f7b63932a7c047b863b10438b
Manuel De La Torre <109108957+Jumas0210@users.noreply.github.com>
2024-12-08 20:20:10 -0500

    Revert "Feature/technologies"

--- src/const/technologies.ts
+++ src/const/technologies.ts
@@ -3,134 +3,91 @@ import type { ITechnologies } from "../interfaces/ITechnologies";
 export const TECHNOLOGIES: ITechnologies[] = [
     {
         name: "Angular",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Angular.svg",
-        type: "front-end",
-        description: "Angular es un framework desarrollado por Google que facilita la construcción de aplicaciones web dinámicas y de gran escala. Su enfoque modular y las herramientas integradas para manejar servicios, rutas y formularios lo hacen ideal para proyectos de alta complejidad. Perfecta para proyectos que requieran aplicaciones robustas con interactividad avanzada y un flujo de datos estructurado."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Angular.svg"
     },
     {
         name: "Astro",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Astro.svg",
-        type: "front-end",
-        description: "Astro es una plataforma para generar sitios estáticos optimizados que priorizan la velocidad y el rendimiento. Permite integrar múltiples frameworks como React o Vue, manteniendo el contenido liviano y rápido. Perfecta para proyectos orientados a la entrega de contenido estático con tiempos de carga mínimos y un rendimiento excepcional."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Astro.svg"
     },
     {
         name: "AWS",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/AWS.svg",
-        type: "herramienta",
-        description: "AWS es un conjunto de servicios en la nube que permite gestionar almacenamiento, bases de datos, despliegue de aplicaciones, análisis y mucho más. Es conocido por su escalabilidad, seguridad y opciones de personalización. Perfecta para proyectos que necesiten soluciones en la nube confiables y escalables."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/AWS.svg"
     },
     {
         name: "Bootstrap",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Bootstrap.svg",
-        type: "front-end",
-        description: "Bootstrap es un framework CSS que simplifica el diseño de interfaces responsivas con componentes predefinidos. Ofrece un sistema de rejilla que facilita la construcción de diseños flexibles y consistentes. Perfecta para proyectos que requieran implementar interfaces atractivas rápidamente con estilos predeterminados."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Bootstrap.svg"
     },
     {
         name: "C#",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/C%23%20(CSharp).svg",
-        type: "back-end",
-        description: "C# es un lenguaje versátil y potente diseñado por Microsoft, utilizado principalmente en aplicaciones de escritorio, servicios web y desarrollo de videojuegos con Unity. Ofrece características avanzadas para aplicaciones empresariales. Perfecta para proyectos que demanden rendimiento, estabilidad y un ecosistema sólido."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/C%23%20(CSharp).svg"
     },
     {
         name: "CSS3",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/CSS3.svg",
-        type: "front-end",
-        description: "CSS3 es el estándar para dar estilo y diseño a las páginas web. Permite implementar transiciones, animaciones, y diseños adaptables, mejorando la experiencia visual. Perfecta para proyectos donde la estética y usabilidad sean esenciales."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/CSS3.svg"
     },
     {
         name: "Firebase",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Firebase.svg",
-        type: "herramienta",
-        description: "Firebase es una plataforma que proporciona servicios backend como bases de datos en tiempo real, hosting y autenticación. Es fácil de integrar y ofrece herramientas para un desarrollo ágil. Perfecta para proyectos que necesiten una solución backend rápida y confiable sin administrar servidores."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Firebase.svg"
     },
     {
         name: "Gatsby",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Gatsby.svg",
-        type: "front-end",
-        description: "Gatsby es un generador de sitios estáticos basado en React que prioriza el rendimiento, la seguridad y la experiencia de usuario. Permite integrar diversas fuentes de datos, como APIs y CMS. Perfecta para proyectos centrados en SEO y velocidad."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Gatsby.svg"
     },
     {
         name: "HTML5",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/HTML5.svg",
-        type: "front-end",
-        description: "HTML5 es el estándar base para estructurar contenido web. Permite integrar medios como audio, video y gráficos de forma nativa sin complementos adicionales. Perfecta para proyectos que necesiten una base sólida y flexible para contenido web."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/HTML5.svg"
     },
     {
         name: "Java",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Java.svg",
-        type: "back-end",
-        description: "Java es un lenguaje multiplataforma ampliamente usado para aplicaciones empresariales, sistemas bancarios y desarrollo móvil en Android. Su enfoque en la robustez y seguridad lo hace ideal para aplicaciones críticas. Perfecta para proyectos de larga duración con necesidades de escalabilidad."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Java.svg"
     },
     {
         name: "JavaScript",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/JavaScript.svg",
-        type: "front-end",
-        description: "JavaScript es un lenguaje de programación esencial para agregar interactividad a los sitios web. Es compatible con todos los navegadores y permite construir aplicaciones dinámicas tanto en el lado cliente como en el servidor. Perfecta para proyectos donde la experiencia de usuario dinámica sea crucial."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/JavaScript.svg"
     },
     {
         name: "Material UI",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Material%20UI.svg",
-        type: "front-end",
-        description: "Material UI es una biblioteca de componentes basada en Material Design que facilita la creación de interfaces visualmente consistentes y modernas. Ofrece componentes altamente personalizables con soporte para temas y estilos adaptativos. Perfecta para proyectos que necesiten interfaces elegantes con un diseño limpio y profesional."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Material%20UI.svg"
     },
     {
         name: "MongoDB",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/MongoDB.svg",
-        type: "herramienta",
-        description: "MongoDB es una base de datos NoSQL orientada a documentos, ideal para manejar datos no estructurados y de rápido crecimiento. Es altamente escalable y ofrece flexibilidad en el modelado de datos. Perfecta para proyectos con datos dinámicos o que necesiten adaptarse a cambios frecuentes en los esquemas."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/MongoDB.svg"
     },
     {
         name: "MySQL",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/MySQL.svg",
-        type: "herramienta",
-        description: "MySQL es un sistema de gestión de bases de datos relacionales conocido por su fiabilidad, facilidad de uso y rendimiento. Es ideal para aplicaciones empresariales, sitios web y sistemas con transacciones complejas. Perfecta para proyectos que requieren bases de datos estructuradas y alto rendimiento en consultas."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/MySQL.svg"
     },
     {
         name: "NestJs",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Nest.js.svg",
-        type: "back-end",
-        description: "NestJs es un framework progresivo para construir aplicaciones de servidor escalables, basándose en TypeScript y aprovechando principios de diseño modular. Proporciona una estructura clara para manejar controladores, servicios y middleware. Perfecta para proyectos backend robustos con una arquitectura bien organizada y escalable."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Nest.js.svg"
     },
     {
         name: "NextJs",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Next.js.svg",
-        type: "front-end",
-        description: "Next.js es un framework basado en React que facilita la creación de aplicaciones web con renderizado del lado del servidor (SSR) y generación de sitios estáticos (SSG). Es conocido por optimizar el rendimiento y mejorar la experiencia del usuario. Perfecta para proyectos que necesiten una combinación de SEO y rendimiento excepcional."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Next.js.svg"
     },
     {
         name: "NodeJs",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Node.js.svg",
-        type: "back-end",
-        description: "Node.js es un entorno de ejecución para JavaScript que permite construir aplicaciones del lado del servidor con alta capacidad de respuesta y escalabilidad. Es ideal para manejar aplicaciones en tiempo real, como chats y servicios de streaming. Perfecta para proyectos que requieran un backend rápido y eficiente basado en JavaScript."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Node.js.svg"
     },
     {
         name: "PostgreSQL",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/PostgresSQL.svg",
-        type: "herramienta",
-        description: "PostgreSQL es un sistema de gestión de bases de datos relacionales de código abierto con características avanzadas como soporte para JSON y consultas complejas. Es confiable, escalable y adecuado para manejar grandes volúmenes de datos. Perfecta para proyectos empresariales con requisitos de bases de datos robustas y flexibles."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/PostgresSQL.svg"
     },
     {
         name: "React",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/React.svg",
-        type: "front-end",
-        description: "React es una biblioteca de JavaScript diseñada para construir interfaces de usuario dinámicas y reutilizables. Facilita la gestión del estado y el renderizado eficiente de componentes. Perfecta para proyectos que necesiten experiencias interactivas y escalables con un alto nivel de personalización."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/React.svg"
     },
     {
         name: "Sass",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Sass.svg",
-        type: "front-end",
-        description: "Sass es un preprocesador de CSS que mejora la escritura de estilos con características como variables, mixins y anidación. Ayuda a mantener un código CSS limpio y organizado. Perfecta para proyectos con estilos complejos que demanden una gestión eficiente del diseño."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Sass.svg"
     },
     {
         name: "Tailwind CSS",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Tailwind%20CSS.svg",
-        type: "front-end",
-        description: "Tailwind CSS es un framework de utilidades CSS que permite construir interfaces personalizadas rápidamente sin depender de clases prediseñadas. Es altamente configurable y elimina la necesidad de sobrescribir estilos predefinidos. Perfecta para proyectos que busquen un diseño totalmente personalizado y consistente."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/Tailwind%20CSS.svg"
     },
     {
         name: "TypeScript",
-        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/TypeScript.svg",
-        type: "herramienta",
-        description: "TypeScript es un superconjunto de JavaScript que agrega tipado estático, ayudando a detectar errores en tiempo de desarrollo y mejorando la mantenibilidad del código. Es compatible con cualquier proyecto JavaScript existente. Perfecta para proyectos de gran escala que necesiten código limpio, robusto y fácil de mantener."
+        image: "https://raw.githubusercontent.com/Juanfeross/extraterrestrial-escape/refs/heads/develop/src/assets/icons/TypeScript.svg"
     }
 ];
+
--- src/interfaces/ITechnologies.ts
+++ src/interfaces/ITechnologies.ts
@@ -1,6 +1,4 @@
 export interface ITechnologies {
     name: string
     image: string
-    type : string
-    description : string
 }
